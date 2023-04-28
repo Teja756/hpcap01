1a8f2f4ef6b6532cd40b2b3c9a85e8015b6ccc55
Teja <tejasbagul23@gmail.com>
2023-04-28 15:46:15 +0530

    update calculator

---
+++ calculator.java
@@ -0,0 +1,16 @@
+public class calculator{
+	public static void main(String[] args){
+	int a=10,b=15,add,mul,div,sub;
+
+    	add = a + b;
+	mul = a * b;
+	div = a / b;
+	sub = a - b;
+         System.out.println("Sum = " + add);
+    System.out.println("Difference = " + sub);
+    System.out.println("Multiplication = " + mul);
+    System.out.println("Division = " + div);
+	
+	
+	}
+}
